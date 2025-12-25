edcf4a67508a78f895ec362be6f1464ad10271ff
Muhammad Ashraf <nayeemashraf92@gmail.com>
2025-12-25 22:36:25 +0500

    database contennet

--- Auth/Authentication/src/app.module.ts
+++ Auth/Authentication/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'register-db',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
